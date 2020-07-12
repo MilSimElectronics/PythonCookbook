873743726a783a209e4fea9bd4b734cc0f8ff546
MilSimElectronics <email2developer@gmail.com>
2020-07-12 12:02:55 -0500

    Added user input and variable type examples.

---
+++ user_input.py
@@ -0,0 +1,7 @@
+print("Hello")
+name = input("What is your name? ")
+print("Hello " + name)
+
+number = input("what is your favorite number? ")
+triple = int(number) * 3
+print("Your number x 3 is: " + str(triple))
